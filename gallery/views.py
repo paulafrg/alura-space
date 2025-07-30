7b5bd208f5ff3af63199d11f3bbbf6d447dd05e5
paulafrg <paula.f.r@outlook.com>
2025-07-29 22:18:27 -0400

    html updated with partials

---
+++ gallery/views.py
@@ -0,0 +1,8 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request,'gallery/index.html')
+
+def imagem(request):
+    return render(request, 'gallery/imagem.html')
